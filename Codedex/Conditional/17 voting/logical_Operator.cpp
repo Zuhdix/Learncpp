1f3af2b3c54c5d621a0a35b098c3eee6c3811f08
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-11-12 23:43:44 +0700

    asli ( refactor )

--- Codedex/Conditional/17 voting/logical_Operator.cpp
+++ Codedex/Conditional/17 voting/logical_Operator.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 
+int main () {
+
+    std::cout << "Enter your age: ";
+    int age {};
+    std::cin >> age;
+
+    std::cout << "are you a citizen ( 1 = yes or 0 = no ): ";
+    bool citizen {};
+    std::cin >> citizen;
+
+    std::cout << "Have you register ( 1 = yes or 0 = no ): ";
+    bool registered {};
+    std::cin >> registered;
+
+
+    if (age >= 18 && citizen && registered) {
+        std::cout << "You can vote!. \n";
+    }
+    else if (age < 18) {
+        std::cout << "You are not old enough to vote. \n";
+    }
+    else if (citizen == false) {
+        std::cout << "You are not eligible to vote. \n";
+    }
+    else if (registered == false) {
+        std::cout << "You must registered first. \n";
+    }
+    else {
+        std::cout << "cannot meet the requirement. \n";
+    }
+    
+    return 0;
+}
