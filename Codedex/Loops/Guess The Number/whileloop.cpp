60e139b27dcdecf5a0077fdc06d086c6d7b1f72e
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-11-16 00:44:18 +0700

    Add number guessing game with while loop
    Implement number guessing game using a while loop.

--- Codedex/Loops/Guess The Number/whileloop.cpp
+++ Codedex/Loops/Guess The Number/whileloop.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+int main () {
+    int answer {}; // Sesuai learncpp, 'answer' diinisialisasi ke 0
+
+    // Pengecekan pertama: while (0 != 64)
+    // Kondisi ini TRUE, jadi loop dimulai.
+    while (answer != 64) {
+        std::cout << "guess the number? ";
+        std::cin >> answer;
+    } 
+    
+    // Ini akan dieksekusi HANYA SETELAH
+    // loop di atas selesai (yaitu, saat answer == 64)
+    std::cout << "You guessed it correct! ";  
+    
+    return 0;
+}
