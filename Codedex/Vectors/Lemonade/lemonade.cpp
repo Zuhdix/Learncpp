7c7def66d0b8782cb59bc5a726474a3bdeb8fc6e
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-11-24 21:41:38 +0700

    Add lemonade.cpp to display lemonade and sales data

---
+++ Codedex/Vectors/Lemonade/lemonade.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main () {
+
+    std::vector<int> lemonade {25, 28, 40, 85, 76};
+    std::vector<double> sales {75.50, 89.25, 60.00, 102.45, 95.10};
+
+    std::cout << lemonade[4] << " \n";  // cara cetak 76 , index dimulai dari 0
+    std::cout << sales[4] << " \n";
+
+    std::cout << lemonade[4] / sales[4] << " \n";
+
+
+
+    return 0;
+}
