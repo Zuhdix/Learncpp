0c2737f93a45dcb8a273962a06fd1fecb02614f3
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-11-24 22:21:59 +0700

    warn doang, kode bisa jalan

---
+++ Codedex/Vectors/Playlist/noflag.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(){
+
+    std::vector<std::string> playlist {
+        "Porches - rangerover",
+        "Mount Eerie - You Swan, Go on",
+        "Yoasobi - Tabun",
+        "Taylor Swift - Blank Space",
+        "Adele - Someone like you",
+        "Ariana Grande - Supranatural"
+    };
+
+    // Ubah int menjadi std::size_t agar "damai" dengan .size()
+    for (std::size_t i = 0; i < playlist.size(); i++) {
+        std::cout << playlist[i] << '\n';
+    }
+
+    return 0;
+}
