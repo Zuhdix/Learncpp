2a63f9c7805bf628918d45766f70e1866e84285c
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-11-28 23:50:32 +0700

    Add menu function to display food options
    Implement food menu display in codedex.cpp

---
+++ Codedex/Function/Food Menu/codedex.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// menampilkan menu
+
+void menu() {
+    std::cout << "Here's the menu: \n";
+    std::cout << "1. 🍔 Cheeseburger \n";
+    std::cout << "2. 🍟 Fries \n";
+    std::cout << "3. 🥤 Soda \n";
+    std::cout << "4. 🍦 Ice Cream \n";
+    std::cout << "5. 🍪 Cookie \n";
+}
+
+int main() {
+    std::cout << "Welcome to Sonnyboy's Diner! \n\n";
+    menu();
+
+    return 0;
+}
