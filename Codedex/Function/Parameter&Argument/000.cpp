66e678f6b2225671c6d4b06fd3f6e69b4204cf47
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-11-29 00:16:08 +0700

    Add out-of-office message function

--- Codedex/Function/Parameter&Argument/000.cpp
+++ Codedex/Function/Parameter&Argument/000.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string>
 
+void ooo_message(int month, int date) {
+    std::cout << "I am currently out of the office and will be back at " << month << "/"<< date << ". \n";
+    std::cout << "I'll respond as soon as I can upon my return. \n";
+}
+
+int main() {
+    ooo_message(4, 28);
+
+    return 0;
+}
