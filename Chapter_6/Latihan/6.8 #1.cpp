176a3284735d75a58bb56f264f08b050d692fdb8
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-01-16 22:47:55 +0700

    quiz time!

---
+++ Chapter_6/Latihan/6.8 #1.cpp
@@ -0,0 +1,21 @@
+quiz time!
+a). (true && true) || false << short sircuit terjadi (operator kiri true)
+    (true || false) jadi true karena or
+
+b). (false && true) || true
+    (false || true) jadi true karena or
+
+c). (false && true) || false || true
+    ((false || false) || true)
+    (false || true) jadi true karena or
+
+d). ( 5 > 6 || 4 > 3) && (7 > 8)
+    (false || 4 > 3) && (7 > 8)
+    (false || true) && (7 > 8)
+    true && (7 > 8)
+    true && false jadi false karena and // ini versi lengkap
+
+e). !(7 > 6 || 3 > 4)
+    !(true || false)
+    !(true)
+    !true = false
