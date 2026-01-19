ff6e817ccac9cafa5d9f73b31d5f7decf61766d7
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-01-20 01:57:54 +0700

    Refactor getQuantityPhrase and getApplesPluralized functions

--- Chapter_6/Latihan/6.x #1.cpp
+++ Chapter_6/Latihan/6.x #1.cpp
@@ -5,31 +5,22 @@ constexpr std::string_view getQuantityPhrase(const int apple)
 {
 	if (apple < 0)
 		return "negative";
-	else if (apple == 0)
+	if (apple == 0)
 		return "no";
-	else if (apple == 1)
+	if (apple == 1)
 		return "a single";
-	else if (apple == 2)
+	if (apple == 2)
 		return "a couple of";
-	else if (apple == 3)
+	if (apple == 3)
 		return "a few";
-	else
-	{
+	
 		return "many";
-	}
-
-
+	
 }
 	
 std::string_view getApplesPluralized(int apple)
 {
-	if (apple == 1)
-		return "apple";
-	else
-	{
-		return "apples";
-	}
-
+	return (apple == 1) ? "apple" : "apples";
 }
 
 int main()
@@ -42,6 +33,5 @@ int main()
 	std::cin >> numApples;
 
 	std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
-
 	return 0;
 }
