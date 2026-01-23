a07574dbbb6917cb80801856f2cb4bf6a1d2dc3c
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-01-23 21:44:08 +0700

    Update switch.cpp

--- Kumpulan kode Algo 1/switch.cpp
+++ Kumpulan kode Algo 1/switch.cpp
@@ -1,23 +1,24 @@
-//#include <iostream>
-//
-//int main()
-//{
-//	std::cout << "Masukkan kode hari: ";
-//	int hari{};
-//	std::cin >> hari;
-//
-//	switch (hari){
-//	case 1:
-//		std::cout << "Senin";
-//		break;
-//	case 2:
-//		std::cout << "Selasa";
-//		break;
-//	case 3:
-//		std::cout << "Rabu";
-//		break;
-//	}
-//
-//
-//	return 0;
-//}
+#include <iostream>
+
+int main()
+{
+	std::cout << "Masukkan kode hari: ";
+	int hari{};
+	std::cin >> hari;
+
+	switch (hari){
+	case 1:
+		std::cout << "Senin";
+		break;
+	case 2:
+		std::cout << "Selasa";
+		break;
+	case 3:
+		std::cout << "Rabu";
+		break;
+	}
+
+
+	return 0;
+
+}
