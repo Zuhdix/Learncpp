90a590f5fa4e54e97770b6b44313c7e10634582b
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-01-23 21:43:57 +0700

    Update for_loop.cpp

--- Kumpulan kode Algo 1/for_loop.cpp
+++ Kumpulan kode Algo 1/for_loop.cpp
@@ -1,15 +1,16 @@
-//#include <iostream>
-//
-//int main()
-//{
-//	std::cout << "angka iterasi.. \n";
-//
-//	int i{};
-//
-//	for (i = 0; i < 10; i++)
-//	{
-//		std::cout << i << '\n';
-//	}
-//
-//	return 0;
-//}
+#include <iostream>
+
+int main()
+{
+	std::cout << "angka iterasi.. \n";
+
+	int i{};
+
+	for (i = 0; i < 10; i++)
+	{
+		std::cout << i << '\n';
+	}
+
+	return 0;
+
+}
