6236e46644426214e7de05afb6a7422417c5a7ba
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-01-23 21:43:30 +0700

    Uncommented do-while loop example in C++

--- Kumpulan kode Algo 1/do_while_loop.cpp
+++ Kumpulan kode Algo 1/do_while_loop.cpp
@@ -1,25 +1,26 @@
-//#include <iostream>
-//
-//int main()
-//{
-//	std::cout << "iterasi while .... \n";
-//	int i{ 0 }; // while harus di inisialisasi
-//
-//	while (i < 20)
-//	{
-//		std::cout << i << '\n'; // statement
-//		i += 2; // iterasi
-//	}
-//	std::cout << "while berakhir disini.\n\n";
-//
-//	std::cout << "sekarang do dan while \n\n";
-//	int x{ 1 }; // inisialisasi
-//	do
-//	{
-//		std::cout << x << '\n'; // statement yang pasti di eksekusi 1 kali
-//		x += 2;					// iterasi
-//	} while (x <= 10);
-//		std::cout << '\n';
-//
-//	return 0;
-//}
+#include <iostream>
+
+int main()
+{
+	std::cout << "iterasi while .... \n";
+	int i{ 0 }; // while harus di inisialisasi
+
+	while (i < 20)
+	{
+		std::cout << i << '\n'; // statement
+		i += 2; // iterasi
+	}
+	std::cout << "while berakhir disini.\n\n";
+
+	std::cout << "sekarang do dan while \n\n";
+	int x{ 1 }; // inisialisasi
+	do
+	{
+		std::cout << x << '\n'; // statement yang pasti di eksekusi 1 kali
+		x += 2;					// iterasi
+	} while (x <= 10);
+		std::cout << '\n';
+
+	return 0;
+
+}
