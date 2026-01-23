fdf4a7fdb5fbc77fea991bff3611892f40285847
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-01-23 21:42:28 +0700

    Uncomment code for 2D array display

--- Kumpulan kode Algo 1/array2Dimensi.cpp
+++ Kumpulan kode Algo 1/array2Dimensi.cpp
@@ -1,22 +1,23 @@
-//#include<iostream>
-//
-//int main()
-//{	
-//	int array2Dim[3][3] // array 2 dimensi umum dalam matriks
-//	{
-//		{1, 2, 3},
-//		{4, 5, 6},
-//		{7, 8, 9}
-//	};
-//
-//
-//	for (int i = 0; i < 3; i++)
-//	{
-//		for (int j = 0; j < 3; j++)
-//		{
-//			std::cout << array2Dim[i][j] << ' ';
-//		}
-//	}
-//
-//	return 0;
-//}
+#include<iostream>
+
+int main()
+{	
+	int array2Dim[3][3] // array 2 dimensi umum dalam matriks
+	{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9}
+	};
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << array2Dim[i][j] << ' ';
+		}
+	}
+
+	return 0;
+
+}
