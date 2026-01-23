3d237b8e2a17cee81d21e1b55791cbfc6cb1d205
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-01-23 21:41:38 +0700

    Uncomment and restore array printing code

--- Kumpulan kode Algo 1/array1Dimensi.cpp
+++ Kumpulan kode Algo 1/array1Dimensi.cpp
@@ -1,13 +1,14 @@
-//#include<iostream>
-//
-//int main()
-//{
-//	int array1Dim[5]{ 7, 3, 9, 12 ,8 };
-//
-//	for (int i = 0; i < 5; i++)
-//	{
-//		std::cout << array1Dim[i] << '\n'; // print ber urut.
-//	}
-//
-//	return 0;
-//}
+#include<iostream>
+
+int main()
+{
+	int array1Dim[5]{ 7, 3, 9, 12 ,8 };
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << array1Dim[i] << '\n'; // print ber urut.
+	}
+
+	return 0;
+
+}
