b2cab5f86a923e2441dd0f6143dffc920aec9fe3
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-02-16 20:31:08 +0700

    Inline Functions

---
+++ Chapter_7/7.9/Inline_Functions.cpp
@@ -0,0 +1,12 @@
+﻿#include"pi.h" // berisi inline function.
+#include<iostream>
+
+double circumference(double radius); // forward declaration
+
+int main()
+{
+	std::cout << pi() << '\n';
+	std::cout << circumference(2.0) << '\n';
+
+	return 0;
+}
---
+++ Chapter_7/7.9/math.cpp
@@ -0,0 +1,6 @@
+#include "pi.h" // pi akan di iinclude dan dicopy disini
+
+double circumference(double radius)
+{
+	return 2.0 * pi() * radius;
+}
