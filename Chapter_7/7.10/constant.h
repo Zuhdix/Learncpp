bc5cba044c3864af5a22b2d1ff58f594b5eaa12f
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-02-16 20:32:01 +0700

    Sharing Global Constant

---
+++ Chapter_7/7.10/constant.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+namespace constant
+{
+	constexpr double pi{ 3.14159 };
+	constexpr double avogadro{ 6.0221413e23 };
+	constexpr double myGravity{ 9.2 };
+}
+
+#endif 
+
+
