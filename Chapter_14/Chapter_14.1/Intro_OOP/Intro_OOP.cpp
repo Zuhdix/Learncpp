946e6c7aeb95437faf70336eb6d388b3e9d5da76
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2026-08-04 23:28:30 +0700

    OOP nih boss

---
+++ Chapter_14/Chapter_14.1/Intro_OOP/Intro_OOP.cpp
@@ -0,0 +1,12 @@
+﻿// Intro_OOP.cpp : Defines the entry point for the application.
+//
+
+#include "Intro_OOP.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
